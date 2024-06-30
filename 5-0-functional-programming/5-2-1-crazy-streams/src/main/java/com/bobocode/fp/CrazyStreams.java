a5734b0b4dea427757381506edf5838c54ad1900
Timur <avlyakulovtimur@gmail.com>
2024-06-30 14:21:05 +0300

    add some tasks

--- 5-0-functional-programming/5-2-1-crazy-streams/src/main/java/com/bobocode/fp/CrazyStreams.java
+++ 5-0-functional-programming/5-2-1-crazy-streams/src/main/java/com/bobocode/fp/CrazyStreams.java
@@ -1,12 +1,14 @@
 package com.bobocode.fp;
 
 import com.bobocode.model.Account;
+import com.bobocode.model.Sex;
 import com.bobocode.util.ExerciseNotCompletedException;
 import lombok.AllArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.Month;
 import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * {@link CrazyStreams} is an exercise class. Each method represent some operation with a collection of accounts that
@@ -22,6 +24,7 @@ import java.util.*;
  */
 @AllArgsConstructor
 public class CrazyStreams {
+
     private Collection<Account> accounts;
 
     /**
@@ -30,7 +33,8 @@ public class CrazyStreams {
      * @return account with max balance wrapped with optional
      */
     public Optional<Account> findRichestPerson() {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream()
+                .max((a1, a2) -> a1.getBalance().compareTo(a2.getBalance()));
     }
 
     /**
@@ -40,7 +44,9 @@ public class CrazyStreams {
      * @return a list of accounts
      */
     public List<Account> findAccountsByBirthdayMonth(Month birthdayMonth) {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream()
+                .filter(a -> a.getBirthday().getMonth().equals(birthdayMonth))
+                .toList();
     }
 
     /**
@@ -50,7 +56,8 @@ public class CrazyStreams {
      * @return a map where key is true or false, and value is list of male, and female accounts
      */
     public Map<Boolean, List<Account>> partitionMaleAccounts() {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream()
+                .collect(Collectors.groupingBy(a -> a.getSex().equals(Sex.MALE)));
     }
 
     /**
@@ -60,7 +67,10 @@ public class CrazyStreams {
      * @return a map where key is an email domain and value is a list of all account with such email
      */
     public Map<String, List<Account>> groupAccountsByEmailDomain() {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream()
+                .collect(Collectors.groupingBy(
+                        a -> a.getEmail().substring(a.getEmail().indexOf("@") + 1)
+                ));
     }
 
     /**
@@ -69,7 +79,9 @@ public class CrazyStreams {
      * @return total number of letters of first and last names of all accounts
      */
     public int getNumOfLettersInFirstAndLastNames() {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream()
+                .mapToInt(a -> a.getFirstName().length() + a.getLastName().length())
+                .sum();
     }
 
     /**
@@ -78,7 +90,10 @@ public class CrazyStreams {
      * @return total balance of all accounts
      */
     public BigDecimal calculateTotalBalance() {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream()
+                .map(a -> a.getBalance())
+                .reduce((balance1, balance2) -> balance1.add(balance2))
+                .orElseThrow();
     }
 
     /**
@@ -87,7 +102,9 @@ public class CrazyStreams {
      * @return list of accounts sorted by first and last names
      */
     public List<Account> sortByFirstAndLastNames() {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream()
+                .sorted(Comparator.comparing(Account::getFirstName).thenComparing(Account::getLastName))
+                .toList();
     }
 
     /**
@@ -97,7 +114,8 @@ public class CrazyStreams {
      * @return true if there is an account that has an email with provided domain
      */
     public boolean containsAccountWithEmailDomain(String emailDomain) {
-        throw new ExerciseNotCompletedException();
+        return accounts.stream()
+                .anyMatch(a -> a.getEmail().contains(emailDomain));
     }
 
     /**
@@ -181,6 +199,4 @@ public class CrazyStreams {
     public Map<Character, Long> getCharacterFrequencyIgnoreCaseInFirstAndLastNames(int nameLengthBound) {
         throw new ExerciseNotCompletedException();
     }
-
 }
-
